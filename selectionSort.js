e0242099fb734896d6ada5b1a02d7e6e6c02ba0c
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-04-12 12:46:11 +0200

    Create selectionSort.js

---
+++ selectionSort.js
@@ -0,0 +1,39 @@
+function swap(a, b, arr) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
+
+function selectionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        swap(i, j, array);
+      }
+    }
+  }
+  return array;
+}
+
+console.log(
+  selectionSort([
+    1,
+    4,
+    2,
+    8,
+    345,
+    123,
+    43,
+    32,
+    5643,
+    63,
+    123,
+    43,
+    2,
+    55,
+    1,
+    234,
+    92,
+  ])
+);
