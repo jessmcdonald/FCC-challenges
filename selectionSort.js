3cbc2462c270e1e6518b641abfe36d7c7050992a
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-04-12 12:47:02 +0200

    Update selectionSort.js

--- selectionSort.js
+++ selectionSort.js
@@ -6,7 +6,6 @@ function swap(a, b, arr) {
 
 function selectionSort(array) {
   for (let i = 0; i < array.length; i++) {
-    let min = i;
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] > array[j]) {
         swap(i, j, array);
